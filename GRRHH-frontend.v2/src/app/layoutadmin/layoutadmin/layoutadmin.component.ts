1a8878e2e1f3e17c89c216d49068fec6a48c69ac
El UN1c0 <133818721+TheN1c0@users.noreply.github.com>
2025-06-26 19:58:17 -0400

    correciones, permisos recargarpágina

--- GRRHH-frontend.v2/src/app/layoutadmin/layoutadmin/layoutadmin.component.ts
+++ GRRHH-frontend.v2/src/app/layoutadmin/layoutadmin/layoutadmin.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-
+import { AuthService } from '../../services/auth.service';
+import { SeguridadService } from '../../services/seguridad.service';
 @Component({
   selector: 'app-layoutadmin',
   templateUrl: './layoutadmin.component.html',
   styleUrl: './layoutadmin.component.scss',
 })
-export class LayoutadminComponent {
+export class LayoutadminComponent implements OnInit {
   sidebarVisible: boolean = true;
+
+  constructor(
+    private authService: AuthService,
+    private seguridadService: SeguridadService
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.obtenerPermisosActuales().subscribe({
+      next: (usuario) => {
+        this.seguridadService.setPermisos(usuario.permisos || {});
+      },
+      error: (err) => {
+        console.warn(
+          '❌ No se pudieron recuperar los permisos al iniciar LayoutAdmin:',
+          err
+        );
+      },
+    });
+  }
 
   toggleSidebar() {
     this.sidebarVisible = !this.sidebarVisible;
